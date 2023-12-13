cd28bdb0b71d90f6d14bc22f35032b956e109bcd
Isaiah Magana <Isaiah2Mag@gmail.com>
2023-12-13 00:33:32 -0500

    Learn how to read into a file
    Learn how to use the fs library to read into the data from a file aswell as print it to the consol

--- starter/index.js
+++ starter/index.js
@@ -1,1 +1,13 @@
 const fs = require('fs');
+
+const textIn = fs.readFileSync('/Users/isaiahm/Developer/Node-Course/1-node-farm/starter/txt/input.txt', 'utf-8');    //Syncronous version of file reading
+console.log(textIn);
+
+/*
+takes two arguments:
+1 - path to file we are reading (avacado text file)
+2 - charachter encoding (utf-8 standard)
+- If not specified a buffer is returned
+
+We are going to read the text about an avacado
+*/
