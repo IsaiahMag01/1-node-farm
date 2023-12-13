9772a63387b6e8b65d7d70d40d9a7b4a5b97c5ba
Isaiah Magana <Isaiah2Mag@gmail.com>
2023-12-13 00:48:32 -0500

    Writing output to a file
    Learn how to write output to a file using the text file we read into earlier

--- starter/index.js
+++ starter/index.js
@@ -11,3 +11,15 @@ takes two arguments:
 
 We are going to read the text about an avacado
 */
+
+
+//Outputing
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+/*
+Const = ES6 alt to using var to create variables
+Will use the ES6 template string to format our string output
+- Syntax:
+    `Text we are adding data to ${outside Data or data we want to format}`
+writeFileSync('path', <outputing to file>)
+*/
