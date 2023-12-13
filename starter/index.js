a28dcec1a4f276bc5e1bbfae731c96b04e46fc17
Isaiah Magana <Isaiah2Mag@gmail.com>
2023-12-13 01:24:40 -0500

    Show the concept of reading files and output order
    Using Non-Blocking Async show how to read a file and the order of operations when a call-back is involved with other code in the same program

--- starter/index.js
+++ starter/index.js
@@ -1,28 +1,30 @@
 const fs = require('fs');
 
-//Blocking Syncronous way
-const textIn = fs.readFileSync('/Users/isaiahm/Developer/Node-Course/1-node-farm/starter/txt/input.txt', 'utf-8');    //Syncronous version of file reading
-//console.log(textIn);
+// // //Blocking Syncronous way
+// // const textIn = fs.readFileSync('/Users/isaiahm/Developer/Node-Course/1-node-farm/starter/txt/input.txt', 'utf-8');    //Syncronous version of file reading
+// // //console.log(textIn);
 
-/*
-takes two arguments:
-1 - path to file we are reading (avacado text file)
-2 - charachter encoding (utf-8 standard)
-- If not specified a buffer is returned
+// // /*
+// // takes two arguments:
+// // 1 - path to file we are reading (avacado text file)
+// // 2 - charachter encoding (utf-8 standard)
+// // - If not specified a buffer is returned
 
-We are going to read the text about an avacado
-*/
+// // We are going to read the text about an avacado
+// // */
 
 
-//Outputing
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-/*
-Const = ES6 alt to using var to create variables
-Will use the ES6 template string to format our string output
-- Syntax:
-    `Text we are adding data to ${outside Data or data we want to format}`
-writeFileSync('path', <outputing to file>)
-*/
+// //Outputing
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// /*
+// Const = ES6 alt to using var to create variables
+// Will use the ES6 template string to format our string output
+// - Syntax:
+//     `Text we are adding data to ${outside Data or data we want to format}`
+// writeFileSync('path', <outputing to file>)
+// */
 
 //Non-Blocking Asyncronous way
+fs.readFile('./txt/start.txt', 'utf-8' , (err, data) => { console.log(data); }); 
+console.log("Im first"); //Ouptut befor the call-back function above because not processing any data
